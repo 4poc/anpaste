432f0da38e4cf4bb3607663a85557da5db9c0ba2
Matthias H <apoc@sixserv.org>
2013-03-11 00:02:06 +0100

    config option for udp proto 4/6

--- app/lib/announce.js
+++ app/lib/announce.js
@@ -5,7 +5,7 @@ var config = require('../../config.json');
 exports.announce = function (text) {
   var message = new Buffer([config.announce.password, config.announce.channel, text].join('|'));
   console.log('send announce udp message: ' + message);
-  var client = dgram.createSocket("udp4");
+  var client = dgram.createSocket(config.announce.udp_proto);
   client.send(message, 0, message.length, config.announce.port, config.announce.host, function(err, bytes) {
     client.close();
   });
